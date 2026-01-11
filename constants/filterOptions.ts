9b1b7a7de824919db4720d4a4e4e9674be4ec11d
OksanaSlonska <slonskayaoksana@gmail.com>
2026-01-11 19:51:23 +0200

    update icon

--- constants/filterOptions.ts
+++ constants/filterOptions.ts
@@ -1,20 +1,6 @@
-// export const vehicleEquipmentFilters = [
-//   { key: "AC", label: "AC", icon: "icon-wind" },
-//   { key: "transmission", label: "Automatic", icon: "icon-diagram" },
-//   { key: "kitchen", label: "Kitchen", icon: "icon-kitchen-new" },
-//   { key: "TV", label: "TV", icon: "icon-tv" },
-//   { key: "bathroom", label: "Bathroom", icon: "icon-ph_shower" },
-// ];
-
-// export const vehicleTypeFilters = [
-//   { key: "Van", label: "Van", icon: "icon-bi_grid-1x2" },
-//   { key: "Fully Integrated", label: "Fully Integrated", icon: "icon-bi_grid" },
-//   { key: "Alcove", label: "Alcove", icon: "icon-bi_grid-3x3-gap" },
-// ];
-
 export const vehicleEquipmentFilters = [
   { key: "AC", label: "AC", icon: "icon-wind" },
-  { key: "kitchen", label: "Kitchen", icon: "icon-cup-hot" },
+  { key: "kitchen", label: "Kitchen", icon: "icon-kitchen-new" },
   { key: "TV", label: "TV", icon: "icon-tv" },
   { key: "bathroom", label: "Bathroom", icon: "icon-ph_shower" },
 ];
